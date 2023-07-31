ad0d6dd9ff359a23e4bc327e9de30f37f7471638
Svitlana Zhovanik <lanasvetcat@gmail.com>
2023-07-31 13:08:38 +0300

    feat: added tab and desk services

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -4,7 +4,7 @@ import { SlideProps } from '@/views/services/Services.interface';
 
 export default function Slide(slide: SlideProps) {
   return (
-    <li className="relative py-[70px] keen-slider__slide">
+    <li className="relative py-[70px] keen-slider__slide md:pt-[126px] md:pb-[131px] xl:pt-[101px] xl:pb-[114px]">
       <Image
         src={slide.bg_image.src}
         alt={slide.bg_image.alt}
@@ -14,29 +14,33 @@ export default function Slide(slide: SlideProps) {
         className="absolute left-0 right-0 top-0 bottom-0 -z-10 object-cover object-center "
       />
       <div className="container">
-        <div className="mb-[15px]">
-          <h2 className="title mb-[23px]">
+        <div className="mb-[15px] md:flex md:justify-between md:items-center md:mb-8 xl:max-w-[883px] xl:mb-6">
+          <h2 className="title smOnly:mb-[23px]">
             {data.title.name}{' '}
             <span className="font-medium">{data.title.span}</span>
           </h2>
-          <p className="text-end font-thin text-[43px] text-white/20 leading-none">
+          <p className="text-end font-thin text-[43px] text-white/20 leading-none md:text-[69px] xl:text-[97px]">
             <span className="text-white">{slide.date.day}</span>
             {slide.date.month}
           </p>
         </div>
-        <Image
-          src={slide.image.src}
-          alt={slide.image.alt}
-          width={280}
-          height={213}
-          className="w-full mb-[11px] md:w-[463px] md:h-[370px] xl:w-[608px] xl:h-[434px] object-cover object-center"
-        />
-        <p className="text-end text-[12px] font-extralight leading-loose tracking-[2.4px] smOnly:mb-[227px]">
-          {slide.description}
-        </p>
-        <p className="text-[14px] font-extralight leading-[1.4]">
-          {slide.text}
-        </p>
+        <div className="md:flex md:justify-between md:items-end mdOnly:gap-5 ">
+          <Image
+            src={slide.image.src}
+            alt={slide.image.alt}
+            width={280}
+            height={213}
+            className="w-full smOnly:mb-[11px] md:w-[463px] md:h-[370px] xl:w-[608px] xl:h-[434px] object-cover object-center"
+          />
+          <div>
+            <p className="smOnly:text-end text-[12px] font-extralight leading-loose tracking-[2.4px] smOnly:mb-[227px] mb-[30px] xl:hidden">
+              {slide.description}
+            </p>
+            <p className="text-[14px] font-extralight leading-[1.4] md:text-[13px] md:leading-[1.5] xl:max-w-[293px] xl:text-[18px] xl:leading-[1.3]">
+              {slide.text}
+            </p>
+          </div>
+        </div>
       </div>
     </li>
   );
--- src/views/services/Services.tsx
+++ src/views/services/Services.tsx
@@ -28,7 +28,7 @@ export default function Services() {
         ))}
       </ul>
       {loaded && instanceRef.current && (
-        <div className="absolute sm:top-[calc(50%+112px)] left-[7%] sm:left-[calc(50%-215px)] flex flex-col text-start gap-[14px] max-w-[211px] uppercase text-[20px] font-extralight leading-[0.8] dots_wrapper">
+        <div className="absolute sm:top-[calc(50%+112px)] left-[7%] sm:left-[calc(50%-215px)] flex flex-col text-start gap-[14px] notXl:max-w-[211px] uppercase text-[20px] font-extralight leading-[0.8] dots_wrapper md:top-[229px] md:left-[calc(50%+130px)] xl:w-[455px] xl:gap-6 xl:left-[calc(50%+50px)]">
           {data.buttons.map((button, idx) => {
             return (
               <button
@@ -40,9 +40,16 @@ export default function Services() {
                   currentSlide === idx
                     ? 'text-white font-medium dot flex items-center gap-2'
                     : 'text-white/50'
-                } text-start uppercase text-[20px] font-extralight leading-[0.8] `}
+                } text-start uppercase text-[20px] font-extralight leading-[0.8] md:text-[22px] xl:text-[28px]-[calc(50%+130px)] xl:w-full`}
               >
                 {button}
+                <span
+                  className={`${
+                    currentSlide === idx ? ' xl:block' : 'xl:hidden'
+                  } hidden xl:text-[12px] xl:font-extralight xl:leading-loose xl:tracking-[2.4px] xl:lowercase xl:ml-auto xl:w-[190px]`}
+                >
+                  {data.slides[idx].description}
+                </span>
               </button>
             );
           })}
