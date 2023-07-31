5cf1dfc92e7006ff8a9b63590647988cab71efa4
Svitlana Zhovanik <lanasvetcat@gmail.com>
2023-07-31 19:50:22 +0300

    fix bugs validation

--- src/views/contact/Contact.tsx
+++ src/views/contact/Contact.tsx
@@ -9,14 +9,22 @@ export default function Contact() {
           {data.title.text}{' '}
           <span className="font-medium">{data.title.span}</span>
         </h2>
-        <div className='xl:flex xl:justify-between'>
+        <div className="xl:flex xl:justify-between">
           <div className="flex flex-col gap-[22px] smOnly:mb-[22px] mdOnly:flex-row mdOnly:gap-[89px] mdOnly:mb-[61px] md:justify-center xl:justify-between">
             <div className="flex flex-col gap-[22px] ">
               <div className="flex justify-end gap-5">
                 <ul className="flex flex-col gap-1 text-[14px] font-normal md:text-[16px] xl:text-[18px]">
                   {data.phone.numbers.map((number, index) => {
-                    return <li key={index}>
-                      <a href={`tel:${number}`} className='duration-300 hover:opacity-40 focus:opacity-40' >{number}</a></li>;
+                    return (
+                      <li key={index}>
+                        <a
+                          href={number.url}
+                          className="duration-300 hover:opacity-40 focus:opacity-40"
+                        >
+                          {number.name}
+                        </a>
+                      </li>
+                    );
                   })}
                 </ul>
                 <p className="text-[12px] leading-[1.6] font-extralight w-[100px]">
@@ -24,7 +32,12 @@ export default function Contact() {
                 </p>
               </div>
               <p className="text-[14px] font-normal flex gap-5 justify-end md:text-[16px] items-center xl:text-[18px]">
-               <a href={`mailto:${data.mail.email}`} className='duration-300 hover:opacity-40 focus:opacity-40' >{data.mail.email}</a>
+                <a
+                  href={`mailto:${data.mail.email}`}
+                  className="duration-300 hover:opacity-40 focus:opacity-40"
+                >
+                  {data.mail.email}
+                </a>
                 <span className="text-[12px] leading-[1.6] font-extralight w-[100px] ">
                   {data.mail.text}
                 </span>
@@ -38,7 +51,12 @@ export default function Contact() {
                       key={index}
                       className="text-[14px] font-normal md:text-[16px] xl:text-[18px]"
                     >
-                      <a href={link.url} target='_blank' rel='noopener noreferrer nofollow' className="duration-300 hover:opacity-40 focus:opacity-40">
+                      <a
+                        href={link.url}
+                        target="_blank"
+                        rel="noopener noreferrer nofollow"
+                        className="duration-300 hover:opacity-40 focus:opacity-40"
+                      >
                         {link.name}
                       </a>
                     </li>
